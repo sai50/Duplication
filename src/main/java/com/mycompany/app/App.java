77b71192f37550a58634e270ede8f227aebd1661
saiteja50 <madabhushisaiteja@gmail.com>
2016-08-27 15:27:59 +0530

    Adding Files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+public class App 
+{
+public static void main(String[] a){
+       
+        try{
+            int i = 10/0;
+        } catch(Exception ex){
+            System.out.println("Inside 1st catch Block");
+        } finally {
+            System.out.println("Inside 1st finally block");
+        }
+        
+        try{
+            int i = 10/10;
+        } catch(Exception ex){
+            System.out.println("Inside 2nd catch Block");
+        } finally {
+            System.out.println("Inside 2nd finally block");
+        }
+    }
+}
